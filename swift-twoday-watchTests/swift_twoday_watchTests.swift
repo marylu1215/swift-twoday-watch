6eed3ead2d066e55854de234b02fe07cf6bc7545
jun xie <iumnyo@163.com>
2019-03-09 10:16:04 +0800

    Initial Commit

---
+++ swift-twoday-watchTests/swift_twoday_watchTests.swift
@@ -0,0 +1,34 @@
+//
+//  swift_twoday_watchTests.swift
+//  swift-twoday-watchTests
+//
+//  Created by MacBook Pro on 2019/3/9.
+//  Copyright © 2019年 MacBookPro. All rights reserved.
+//
+
+import XCTest
+@testable import swift_twoday_watch
+
+class swift_twoday_watchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
